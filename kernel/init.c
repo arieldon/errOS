876fefb2025a32911aceaf60034b464e902ac90a
Ariel Don <ariel@arieldon.com>
2021-10-21 14:18:25 -0500

    Print message from kernel land

--- kernel/init.c
+++ kernel/init.c
@@ -1,5 +1,36 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#define VGA_WIDTH	80
+#define VGA_HEIGHT	25
+
+#define BLACK	 0
+#define WHITE	15
+
+void
+clear(void)
+{
+	uint16_t *vgamem = (uint16_t *)0xb8000;
+	for (size_t row = 0; row < VGA_HEIGHT; ++row) {
+		for (size_t col = 0; col < VGA_WIDTH; ++col) {
+			vgamem[row * VGA_WIDTH + col] = ' ' | (uint16_t) BLACK << 8;
+		}
+	}
+}
+
+void
+print(char *str)
+{
+	uint16_t *vgamem = (uint16_t *)0xb8000;
+	for (size_t i = 0; str[i] != '\0'; ++i) {
+		vgamem[i] = str[i] | (uint16_t) WHITE << 8;
+	}
+}
+
 void
 main(void)
 {
+	clear();
+	print("Enter kernel land.");
 	for (;;) {}
 }
