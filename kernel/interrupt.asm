0354b8a8049881ab78ea6ab2dbca185af3466bc0
Ariel Don <ariel@arieldon.com>
2021-11-03 19:42:57 -0500

    Differentiate ISRs that provide error codes

--- kernel/interrupt.asm
+++ kernel/interrupt.asm
@@ -1,9 +1,18 @@
-%macro	ISR 1
+%macro	ISR_NOERR 1
 	global	isr%1
 isr%1:
 	cli
-	push	0
-	push	%1
+	push	0		; No error code to be sent for these ISRs.
+	push	%1		; Push the interrupt number.
+	jmp	isr_common_stub
+%endmacro
+
+
+%macro	ISR_ERR 1		; These ISRs provide error based on Intel ISA.
+	global isr%1
+isr%1:
+	cli
+	push	%1		; Push only the interrupt number.
 	jmp	isr_common_stub
 %endmacro
 
@@ -34,35 +43,35 @@ isr_common_stub:
 	iret
 
 
-ISR 0
-ISR 1
-ISR 2
-ISR 3
-ISR 4
-ISR 5
-ISR 6
-ISR 7
-ISR 8
-ISR 9
-ISR 10
-ISR 11
-ISR 12
-ISR 13
-ISR 14
-ISR 15
-ISR 16
-ISR 17
-ISR 18
-ISR 19
-ISR 20
-ISR 21
-ISR 22
-ISR 23
-ISR 24
-ISR 25
-ISR 26
-ISR 27
-ISR 28
-ISR 29
-ISR 30
-ISR 31
+ISR_NOERR	0
+ISR_NOERR	1
+ISR_NOERR	2
+ISR_NOERR	3
+ISR_NOERR	4
+ISR_NOERR	5
+ISR_NOERR	6
+ISR_NOERR	7
+ISR_ERR	8
+ISR_NOERR	9
+ISR_ERR	10
+ISR_ERR	11
+ISR_ERR	12
+ISR_ERR	13
+ISR_ERR	14
+ISR_NOERR	15
+ISR_NOERR	16
+ISR_ERR	17
+ISR_NOERR	18
+ISR_NOERR	19
+ISR_NOERR	20
+ISR_ERR	21
+ISR_NOERR	22
+ISR_NOERR	23
+ISR_NOERR	24
+ISR_NOERR	25
+ISR_NOERR	26
+ISR_NOERR	27
+ISR_NOERR	28
+ISR_NOERR	29
+ISR_ERR	30
+ISR_NOERR	31
